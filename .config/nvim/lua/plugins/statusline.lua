d3b02f11803b2c348989d60bd86fe5406c7502b7
Johannes Kresner <johannes@jokresner.de>
2024-12-17 21:58:20 +0100

    feat: add nvim as submodule

--- .config/nvim/lua/plugins/statusline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"echasnovski/mini.statusline",
-	opts = function()
-		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
-	end,
-}
-
--- vim: ts=2 sts=2 sw=2 et
